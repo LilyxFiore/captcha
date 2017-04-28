609a6a2a5aa5be0d23cdfe313a23f0b40f20341a
Liliana Fiore <liliana.fiorela@gmail.com>
2017-04-28 13:16:05 -0500

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+var captcha = document.getElementById('captcha');
+var recaptcha = document.getElementById("recaptcha");
+var check = document.getElementById("validate");
+var result = document.getElementById("result");
+var textCaptcha = document.getElementById("text");
+
+window.addEventListener("load", generatorCaptcha);
+recaptcha.addEventListener("click", generatorCaptcha);
+
+function generatorCaptcha(){
+  var num1 = Math.round(Math.random() * 9);
+  var letra1 = String.fromCharCode(Math.round(Math.random() * 25) + 65);  //[fromCharCode]Convierte numero unicode a caracteres
+  var num2 = Math.round(Math.random() * 9);
+  var letra2 = String.fromCharCode(Math.round(Math.random() * 25) + 65);
+  var num3 = Math.round(Math.random() * 9);
+  var letra3 = String.fromCharCode(Math.round(Math.random() * 25) + 65);
+  var num4 = Math.round(Math.random() * 9);
+  var color1 = Math.round(Math.random() * 255);
+  var color2 = Math.round(Math.random() * 255);
+  var color3 = Math.round(Math.random() * 255);
+  captcha.setAttribute("style","color:rgb("+color1+","+color2+","+color3+")");
+  captcha.value = num1+letra1+num2+letra2+num3+letra3+num4;
+}
+
+check.addEventListener("click", function(){
+  if(textCaptcha.value!=captcha.value){
+    console.log(textCaptcha.value);
+    console.log(captcha.value);
+    result.innerHTML = "El captcha introducIDO es incorrecto, intente de nuevo";
+  }
+  else {
+    result.innerHTML="";
+  }
+})
